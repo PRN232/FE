15bc3a6affd71801d6734a70d215953b44751c2b
Hyudeque <quanghuy01062004@gmail.com>
2025-07-21 14:17:17 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,14 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-    webpack: (config) => {
-        config.resolve.alias = {
-            ...(config.resolve.alias || {}),
-            '@': path.resolve(__dirname),
-        };
-        return config;
-    },
+  output: "standalone",
+  webpack: (config, { isServer }) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "~": path.join(__dirname, "src"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
