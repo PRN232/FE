5a9a11ca2b197f0a694fe7c4da17ecf8626725a6
Hyudeque <quanghuy01062004@gmail.com>
2025-07-22 18:40:41 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,18 @@
-import path from "path";
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import path from 'path';
+import fs from 'fs';
+
+const srcPath = path.join(__dirname, 'src');
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      "@": path.resolve(__dirname)
-    };
+  output: 'standalone',
+  webpack(config, { isServer }) {
+    if (fs.existsSync(srcPath)) {
+      config.resolve.alias = {
+        ...(config.resolve.alias || {}),
+        '@': srcPath,
+      };
+    }
     return config;
   },
 };
