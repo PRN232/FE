b1a665c7276889f2a19e9d3631c7991002efd1f1
Hyudeque <quanghuy01062004@gmail.com>
2025-07-23 08:19:17 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,15 @@
 import type { NextConfig } from 'next';
 import path from 'path';
-import fs from 'fs';
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  webpack(config) {
+  webpack(config, _options) {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
-      '@': process.cwd(),
+      '@': path.resolve(__dirname), // project root
     };
     return config;
   },
 };
-
 
 export default nextConfig;
